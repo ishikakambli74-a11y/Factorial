7bdc4898b70d4bea34043e5622469db744ab78c4
ishikakambli74-a11y <ishikakambli74@gmail.com>
2026-02-24 09:27:54 +0530

    factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,5 @@
 def factorial(n):
-    if n = 0:
+    if n < 0:
         return "Factorial not defined for negative numbers"
     elif n == 0 or n == 1:
         return 1
@@ -8,5 +8,5 @@ def factorial(n):
 
 
 if __name__ == "__main__":
-    num = -5   # change this number to test
+    num = 5   # change this number to test
     print(f"Factorial of {num} is {factorial(num)}")
